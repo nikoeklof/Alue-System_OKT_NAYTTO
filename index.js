99da7cd89becca561cc288edc21ac8b7221a583f
unknown <jannemieskolainen@gmail.com>
2023-03-13 09:01:47 +0200

    alku

--- index.js
+++ index.js
@@ -1,12 +1,82 @@
-const http = require('http')
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
 
-console.log("Hello world!")
+let areas = [
+  {
+    id: "1",
+    name: "Pohjoinen",
+    cordinates: { lan: "0", lon: "0" },
+    zone: null,
+    buildings: 50,
+    info: "info",
+    shared: {
+      isTrue: false,
+      ownerId: null,
+      shareDate: null
+    }
+  }, {
+    id: "1",
+    name: "Etelä",
+    cordinates: { lan: "0", lon: "0" },
+    zone: null,
+    buildings: 25,
+    info: null,
+    shared: {
+      isTrue: true,
+      ownerId: "1",
+      shareDate: "0.0.0000"
+    }
+  }
+]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+let owners = [
+  {
+    id: "1",
+    name: "Mr. X",
+    email: "X@isComingForYou.com",
+  }
+]
+
+const typeDefs = `
+  type Area {
+    id: ID!
+    name: String
+    cordinates: {lan: String, lon: String}
+    zone: String
+    buildings Int!
+    info: String
+    shared: {
+      isTrue: Boolean!
+      ownerId: String
+      sharedDate: String
+    }
+  }
+
+  type Owner {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    areasCount: Int!
+    allAreas: [Areas!]!
+    findAreaByName(name: String!): Area!
+  }
+`
+
+const resolvers = {
+  Query: {
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})

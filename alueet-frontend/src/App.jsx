1f40d70dcc51af03d3ba9ae2a7406ca0cddf4767
JojoSnow <milzwow94@gmail.com>
2023-03-14 13:46:46 +0200

    alueiden nimet taulukko aloiteltu

--- alueet-frontend/src/App.jsx
+++ alueet-frontend/src/App.jsx
@@ -5,18 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import NavBar from'./components/NavBar';
 import Register from './components/Register';
+import Areas from './components/Areas';
 
 const App = () => {
 	return (
 		<Router>
 			<Container>
 				<NavBar />
-				<Register />
+				<Areas />
 			</Container>
 
 			<Routes>
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
+				<Route path='/areas' element={<Areas />} />
 			</Routes>
 		</Router>
 		
--- alueet-frontend/src/theme.js
+++ alueet-frontend/src/theme.js
@@ -1,11 +1,15 @@
 const theme = {
 	bgColor: {
 		primary: '#e8e8e8',
-		secondary: '#bdbdbd'
+		secondary: '#bdbdbd',
+		default: '#f7f7f7',
+		blue: '#4dabf5'
 	},
 	color: {
 		primary: '#454545',
-		secondary: '#666666'
+		secondary: '#666666',
+		blue: '#1976d2',
+		error: '#d32f2f'
 	}
 }
 

2f23a9eeff350f3a04463aa73190f58918199887
JojoSnow <milzwow94@gmail.com>
2023-03-16 13:35:17 +0200

    areaControl päivitetty

--- alueet-frontend/src/App.jsx
+++ alueet-frontend/src/App.jsx
@@ -5,20 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import NavBar from'./components/NavBar';
 import Register from './components/Register';
-import Areas from './components/Areas';
+import AreaControl from './components/AreaControl';
 
 const App = () => {
 	return (
 		<Router>
 			<Container>
 				<NavBar />
-				<Areas />
 			</Container>
 
 			<Routes>
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
-				<Route path='/areas' element={<Areas />} />
+				<Route path='/areaControl' element={<AreaControl />} />
 			</Routes>
 		</Router>
 		

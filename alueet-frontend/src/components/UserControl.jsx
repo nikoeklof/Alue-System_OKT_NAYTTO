2789b8f82fed7f4e91717acbd733c5ac76ea1b56
JojoSnow <milzwow94@gmail.com>
2023-03-21 10:39:31 +0200

    melkein valmis, alueet pöytä pitää viimeistellä

--- alueet-frontend/src/components/UserControl.jsx
+++ alueet-frontend/src/components/UserControl.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import {
+	Box,
+	Button,
+	Collapse,
 	Container,
-	Typography,
+	IconButton,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TablePagination,
+	TableRow,
+	Typography
 } from '@mui/material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+
 import theme from '../theme';
+import { users } from '../db';
 
 const styles = {
 	mainText: {
@@ -14,15 +29,176 @@ const styles = {
 		mb: 2,
 		pb: 2
 	},
+	button: {
+		m: 1
+	}
+};
+
+const columns = [
+	{
+		id: 'name',
+		label: 'Nimi',
+		minWidth: 170
+	},
+	{
+		id: 'email',
+		label: 'Sähköposti',
+		minWidth: 170
+	},
+	{
+		id: 'quest',
+		label: 'Vieras',
+		minWidth: 50,
+		align: 'right'
+	},
+	{
+		id: 'admin',
+		label: 'Admin',
+		minWidth: 50,
+		align: 'right'
+	},
+	{
+		id: 'id',
+		label: 'ID',
+		minWidth: 60,
+		align: 'right'
+	},
+];
+
+const Row = (value) => {
+	const [open, setOpen] = useState(false);
+	const user = value.user;
+
+	return (
+		<Fragment key={user.id}>
+			<TableRow 
+				hover 
+				key={user.id}
+				sx={{ '& > *': { borderBottom: 'unset' } }}
+			>
+				<TableCell>
+					<IconButton
+						aria-label='expand row'
+						size='small'
+						onClick={() => setOpen(!open)}
+					>
+						{open ? <ExpandLess /> : <ExpandMore />}
+					</IconButton>
+				</TableCell>
+				<TableCell>{user.username}</TableCell>
+				<TableCell>{user.email}</TableCell>
+				<TableCell align='right'>
+					{user.username ? 'Ei' : 'Kyllä'}
+				</TableCell>
+				<TableCell align='right'>
+					{user.admin ? 'Kyllä' : 'Ei'}
+				</TableCell>
+				<TableCell align='right'>{user.id}</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell sx={{py: 0}} colSpan={6}>
+					<Collapse in={open} timeout='auto' unmountOnExit>
+						<Box sx={{margin: 1}}>
+							<Typography 
+								variant='h6'
+								gutterBottom
+								component='div'
+							>
+								Alueet
+							</Typography>
+							<Table size='small' aria-label='areas'>
+								<TableHead>
+									<TableRow>
+										<TableCell>Nimi</TableCell>
+										<TableCell>ID</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{/* fix to have actual info from the object user.areas */}
+									<TableRow key={user.areas.id}>
+										<TableCell 
+											component='th' 
+											scope='row'
+										>
+											Fetch from backend
+										</TableCell>
+										<TableCell>
+											{user.areas.id}
+										</TableCell>
+									</TableRow>
+								</TableBody>
+							</Table>
+							<Button 
+								variant='contained'
+								sx={styles.button}
+							>
+								Muokkaa
+							</Button> 
+						</Box>
+					</Collapse>
+				</TableCell>
+			</TableRow>
+		</Fragment>	
+	)
 };
 
 const UserControl = () => {
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
+
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
+
+	const handleChangeRowsPerPage = (event) => {
+		setRowsPerPage(+event.target.value);
+		setPage(0);
+	};
 
 	return (
 		<Container>
 			<Typography variant='h6' sx={styles.mainText}>
 				Käyttäjien hallinta
 			</Typography>
+
+			<Paper sx={{ width: '100%', overflow: 'hidden' }}>
+				<TableContainer sx={{ maxHeight: 440 }}>
+					<Table stickyHeader aria-label='sticky label'>
+						<TableHead>
+							<TableRow>
+								<TableCell />
+								{columns.map((column) => (
+									<TableCell
+										key={column.id}
+										align={column.align}
+										style={{ minWidth: column.minWidth }}
+									>
+										{column.label}
+									</TableCell>
+								))}
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{users
+								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+								.map((user) => (
+									<Row key={user.id} user={user} />
+								))	
+							}
+						</TableBody>
+					</Table>
+				</TableContainer>
+				<TablePagination
+					rowsPerPageOptions={[5, 10, 25, 50, 100]}
+					component='div'
+					count={users.length}
+					rowsPerPage={rowsPerPage}
+					page={page}
+					labelRowsPerPage='Rivejä per sivu:'
+					onPageChange={handleChangePage}
+					onRowsPerPageChange={handleChangeRowsPerPage}
+				/>
+			</Paper>
 		</Container>
 		
 	)

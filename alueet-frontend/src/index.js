da20d648ac6b608b42d441ff512f27ee4a05de80
Makko420 <laidinen.marco@gmail.com>
2023-03-23 09:30:32 +0200

    queries lisätty frontendiin

--- alueet-frontend/src/index.js
+++ alueet-frontend/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
-ReactDOM.render(
-	<App />, 
-	document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: 'http://localhost:3001',
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)

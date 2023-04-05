6f456e9acb17e1ca0175ea6f5fb8601783511b4e
JojoSnow <milzwow94@gmail.com>
2023-04-05 11:14:44 +0300

    Footer tehty, runko ja perusgrafiikat

---
+++ alueet-frontend/src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+	Paper,
+	Typography,
+} from '@mui/material';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+
+const styles = {
+	footer: {
+		position: 'fixed',
+		bottom: 0,
+		left: 0,
+		right: 0,
+		fontSize: 10
+	},
+	text: {
+		float: 'right',
+		mr: 2,
+		mt: 1
+	},
+	icon: {
+		float: 'right',
+		my: 1,
+	}
+}
+
+const Footer = () => {
+	return (
+		<Paper sx={styles.footer} elevation={4}>
+			<Typography sx={styles.text}>
+				Esedu
+			</Typography>
+			<CopyrightIcon sx={styles.icon} />
+		</Paper>
+	)
+};
+
+export default Footer;

01ad2bebd2da111f587c944394f4eb770863c0ce
ElyAbsolutely <jannemieskolainen@gmail.com>
2023-04-06 11:13:06 +0300

    Revert "Added testing code for Apollo"
    This reverts commit 73ed10270a3c8d5bfb896abc26c11ff5ff02df43.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Container } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-//for testing
-import { useMutation, useQuery } from '@apollo/client'
-import { ALL_AREAS, EDIT_AREA } from './queries'
-
 import Login from './components/Login';
 import NavBar from'./components/NavBar';
 import Register from './components/Register';
@@ -13,37 +9,10 @@ import AreaControl from './components/AreaControl';
 import UserControl from './components/UserControl';
 
 const App = () => {
-
-		//for testing start
-		const result = useQuery(ALL_AREAS)
-		const [ editArea ] = useMutation(EDIT_AREA, {
-			refetchQueries: [ { query: ALL_AREAS }] 
-		})
-		if (result.loading)  {
-			return <div>loading...</div>
-		}
-		//variables for testing
-		const areaId = result.data.allAreas[0].id
-		const type = "Kaupunki"
-		const map = {
-			coordinates: { lan: "62.324", lon: "5.824" },
-			zone: "756 196 123 156"
-		}
-		//click handlers
-		const handle = () =>  editArea({ variables: { areaId, map, type } })
-		const update = () =>  console.log(result)
-		//end
-
 	return (
 		<Router>
 			<Container>
 				<NavBar />
-				<button onClick={handle}>
-					mutate
-				</button>
-				<button onClick={update}>
-					console log result
-				</button>
 			</Container>
 
 			<Routes>

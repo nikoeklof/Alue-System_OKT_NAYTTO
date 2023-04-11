7a7deb9b0739e22c3e2b56425dd8f7d965ae0e4b
JojoSnow <milzwow94@gmail.com>
2023-04-11 10:26:28 +0300

    tiedot lisätty ja napit toimii, main

--- alueet-frontend/src/Main.jsx
+++ alueet-frontend/src/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {
 	Container,
 	Typography,
 	Grid,
-	Button
+	Button,
+	List,
+	ListItem,
+	ListItemText,
+	ListItemIcon,
 } from '@mui/material';
+import MapIcon from '@mui/icons-material/Map';
+import PersonIcon from '@mui/icons-material/Person';
 
 import theme from './style/theme';
 
@@ -24,17 +31,23 @@ const styles = {
 		borderColor: theme.bgColor.secondary,
 		borderWidth: 1,
 		mt: 4,
-		mb: 2,
 		pb: 1
 	},
+	listItem: {
+		m: 0
+	},
 	text: {
-		mb: 1
+		my: 2,
+		ml: 1
 	},
 	box: {
 		px: 2,
 	},
 	button: {
 		m: 0.5
+	},
+	link: {
+		textDecoration: 'none'
 	}
 };
 
@@ -51,58 +64,109 @@ const Main = () => {
 						Alueiden hallinta
 					</Typography>
 					<Grid container sx={styles.box}>
-						<Grid item>
-							<Typography sx={styles.text}>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis sem eget dui pretium, eget suscipit dui malesuada. Aliquam interdum eget nunc in vestibulum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum condimentum vitae turpis sed consectetur. Praesent consequat nunc arcu, vitae blandit felis lobortis.
-							</Typography>
+						<Grid item md={12} xs={12}>
+							<List>
+								<ListItem sx={styles.listItem}>
+									<ListItemIcon>
+										<MapIcon />
+									</ListItemIcon>
+									<ListItemText primary='Tarkastele alueita' />
+								</ListItem>
+								<ListItem>
+									<ListItemIcon>
+										<MapIcon />
+									</ListItemIcon>
+									<ListItemText primary='Luo alueita' />
+								</ListItem>
+								<ListItem>
+									<ListItemIcon>
+										<MapIcon />
+									</ListItemIcon>
+									<ListItemText primary='Muokkaa alueita' />
+								</ListItem>
+								<ListItem>
+									<ListItemIcon>
+										<MapIcon />
+									</ListItemIcon>
+									<ListItemText primary='Lainaa ja palauta alueita' />
+								</ListItem>
+							</List>
 						</Grid>
-						<Grid item>
-							<Button
-								variant='contained'
-								sx={styles.button}
-							>
-								Siirry
-							</Button>
+						<Grid item md={12} xs={12}>
+							<Link to={'/areaControl'} style={styles.link}>
+								<Button
+									variant='contained'
+									sx={styles.button}
+								>
+									Siirry
+								</Button>
+							</Link>
 						</Grid>
 					</Grid>
 					<Typography variant='h6' sx={styles.subText}>
 						Käyttäjien hallinta
 					</Typography>
 					<Grid container sx={styles.box}>
-						<Grid item>
-							<Typography sx={styles.text}>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis sem eget dui pretium, eget suscipit dui malesuada. Aliquam interdum eget nunc in vestibulum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum condimentum vitae turpis sed consectetur. Praesent consequat nunc arcu, vitae blandit felis lobortis.
-							</Typography>
+						<Grid item md={12} xs={12}>
+							<List>
+								<ListItem>
+									<ListItemIcon>
+										<PersonIcon />
+									</ListItemIcon>
+									<ListItemText primary='Tarkastele käyttäjiä' />
+								</ListItem>
+								<ListItem>
+									<ListItemIcon>
+										<PersonIcon />
+									</ListItemIcon>
+									<ListItemText primary='Luo käyttäjiä' />
+								</ListItem>
+								<ListItem>
+									<ListItemIcon>
+										<PersonIcon />
+									</ListItemIcon>
+									<ListItemText primary='Muokkaa ja poista käyttäjiä' />
+								</ListItem>
+							</List>
 						</Grid>
-						<Grid item>
-							<Button
-								variant='contained'
-								sx={styles.button}
-							>
-								Siirry
-							</Button>
+						<Grid item md={12} xs={12}>
+							<Link to={'/userControl'} style={styles.link}>
+								<Button
+									variant='contained'
+									sx={styles.button}
+								>
+									Siirry
+								</Button>
+							</Link>
 						</Grid>
 					</Grid>
 				</Grid>
 				<Grid item md={4} xs={12}>
 					<Typography variant='h6' sx={styles.subText}>
-						Seuraavaksi
+						Aloitus
 					</Typography>
 					<Typography sx={styles.text}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis sem eget dui pretium, eget suscipit dui malesuada. Aliquam interdum eget nunc in vestibulum.
+						Etkö ole vielä rekisteröitynyt?
 					</Typography>
-					<Button
-						variant='contained'
-						sx={styles.button}
-					>
-						Rekisteröidy
-					</Button>
-					<Button
-						variant='contained'
-						sx={styles.button}
-					>
-						Kirjaudu sisään
-					</Button>
+					<Link to={'/register'} style={styles.link}>
+						<Button
+							variant='contained'
+							sx={styles.button}
+						>
+							Rekisteröidy
+						</Button>
+					</Link>
+					<Typography sx={styles.text}>
+						Kirjaudu sisään jos olet jo rekisteröitynyt!
+					</Typography>
+					<Link to={'/login'} style={styles.link}>
+						<Button
+							variant='contained'
+							sx={styles.button}
+						>
+							Kirjaudu sisään
+						</Button>
+					</Link>
 				</Grid>
 			</Grid>
 		</Container>

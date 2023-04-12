c209742762cb0ffdf9aa8b79ccd7ab4f0f0161f3
JojoSnow <milzwow94@gmail.com>
2023-04-12 12:41:09 +0300

    Main update

--- alueet-frontend/src/Main.jsx
+++ alueet-frontend/src/Main.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
 	Container,
+	Collapse,
 	Typography,
 	Grid,
 	Button,
@@ -9,9 +10,17 @@ import {
 	ListItem,
 	ListItemText,
 	ListItemIcon,
+	Paper,
+	IconButton,
+	Box,
+	Divider,
 } from '@mui/material';
-import MapIcon from '@mui/icons-material/Map';
-import PersonIcon from '@mui/icons-material/Person';
+import {
+	Map as MapIcon,
+	Person as PersonIcon,
+	Remove as RemoveIcon,
+	Add as AddIcon,
+} from '@mui/icons-material';
 
 import theme from './style/theme';
 
@@ -23,14 +32,10 @@ const styles = {
 		borderBottom: 'solid',
 		borderColor: theme.bgColor.secondary,
 		borderWidth: 1,
-		mt: 4,
+		my: 4,
 		pb: 2,
 	},
 	subText: {
-		borderBottom: 'solid',
-		borderColor: theme.bgColor.secondary,
-		borderWidth: 1,
-		mt: 4,
 		pb: 1,
 	},
 	listItem: {
@@ -49,9 +54,24 @@ const styles = {
 	link: {
 		textDecoration: 'none',
 	},
+	paper: {
+		p: 1,
+		mb: 1,
+	},
+	icon: {
+		float: 'right',
+	},
+	divider: {
+		borderColor: theme.bgColor.secondary,
+	},
 };
 
 const Main = () => {
+	const [areaControlChecked, setAreaControlChecked] = useState(false);
+	const [userChecked, setUserChecked] = useState(false);
+	const [createAreaChecked, setCreateAreaChecked] = useState(false);
+	const [lendChecked, setLendChecked] = useState(false);
+
 	return (
 		<Container sx={styles.container}>
 			<Typography
@@ -69,160 +89,301 @@ const Main = () => {
 					md={8}
 					xs={12}
 				>
-					<Typography
-						variant='h6'
-						sx={styles.subText}
-					>
-						Alueiden hallinta
-					</Typography>
-					<Grid
-						container
-						sx={styles.box}
-					>
-						<Grid
-							item
-							md={12}
-							xs={12}
+					<Paper sx={styles.paper}>
+						<Box
+							onClick={() =>
+								setAreaControlChecked(!areaControlChecked)
+							}
 						>
-							<List>
-								<ListItem sx={styles.listItem}>
-									<ListItemIcon>
-										<MapIcon />
-									</ListItemIcon>
-									<ListItemText primary='Tarkastele alueita' />
-								</ListItem>
-								<ListItem>
-									<ListItemIcon>
-										<MapIcon />
-									</ListItemIcon>
-									<ListItemText primary='Luo alueita' />
-								</ListItem>
-								<ListItem>
-									<ListItemIcon>
-										<MapIcon />
-									</ListItemIcon>
-									<ListItemText primary='Muokkaa alueita' />
-								</ListItem>
-								<ListItem>
-									<ListItemIcon>
-										<MapIcon />
-									</ListItemIcon>
-									<ListItemText primary='Lainaa ja palauta alueita' />
-								</ListItem>
-							</List>
-						</Grid>
-						<Grid
-							item
-							md={12}
-							xs={12}
-						>
-							<Link
-								to={'/areaControl'}
-								style={styles.link}
+							<Typography
+								variant='h6'
+								sx={styles.subText}
 							>
-								<Button
-									variant='contained'
-									sx={styles.button}
-								>
-									Siirry
-								</Button>
-							</Link>
-						</Grid>
-					</Grid>
-					<Typography
-						variant='h6'
-						sx={styles.subText}
-					>
-						Käyttäjien hallinta
-					</Typography>
-					<Grid
-						container
-						sx={styles.box}
-					>
-						<Grid
-							item
-							md={12}
-							xs={12}
-						>
-							<List>
-								<ListItem>
-									<ListItemIcon>
-										<PersonIcon />
-									</ListItemIcon>
-									<ListItemText primary='Tarkastele käyttäjiä' />
-								</ListItem>
-								<ListItem>
-									<ListItemIcon>
-										<PersonIcon />
-									</ListItemIcon>
-									<ListItemText primary='Luo käyttäjiä' />
-								</ListItem>
-								<ListItem>
-									<ListItemIcon>
-										<PersonIcon />
-									</ListItemIcon>
-									<ListItemText primary='Muokkaa ja poista käyttäjiä' />
-								</ListItem>
-							</List>
-						</Grid>
-						<Grid
-							item
-							md={12}
-							xs={12}
-						>
-							<Link
-								to={'/userControl'}
-								style={styles.link}
+								Alueiden hallinta
+								<IconButton sx={styles.icon}>
+									{areaControlChecked ? (
+										<RemoveIcon />
+									) : (
+										<AddIcon />
+									)}
+								</IconButton>
+							</Typography>
+						</Box>
+
+						<Collapse in={areaControlChecked}>
+							<Divider sx={styles.divider} />
+							<Grid
+								container
+								sx={styles.box}
 							>
-								<Button
-									variant='contained'
-									sx={styles.button}
+								<Grid
+									item
+									md={12}
+									xs={12}
 								>
-									Siirry
-								</Button>
-							</Link>
-						</Grid>
-					</Grid>
+									<List>
+										<ListItem sx={styles.listItem}>
+											<ListItemIcon>
+												<MapIcon />
+											</ListItemIcon>
+											<ListItemText primary='Tarkastele alueita' />
+										</ListItem>
+										<ListItem>
+											<ListItemIcon>
+												<MapIcon />
+											</ListItemIcon>
+											<ListItemText primary='Muokkaa alueita' />
+										</ListItem>
+										<ListItem>
+											<ListItemIcon>
+												<MapIcon />
+											</ListItemIcon>
+											<ListItemText primary='Lainaa ja palauta alueita' />
+										</ListItem>
+									</List>
+								</Grid>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<Link
+										to={'/areaControl'}
+										style={styles.link}
+									>
+										<Button
+											variant='contained'
+											sx={styles.button}
+										>
+											Siirry
+										</Button>
+									</Link>
+								</Grid>
+							</Grid>
+						</Collapse>
+					</Paper>
+
+					<Paper sx={styles.paper}>
+						<Box onClick={() => setUserChecked(!userChecked)}>
+							<Typography
+								variant='h6'
+								sx={styles.subText}
+							>
+								Käyttäjien hallinta
+								<IconButton sx={styles.icon}>
+									{userChecked ? <RemoveIcon /> : <AddIcon />}
+								</IconButton>
+							</Typography>
+						</Box>
+
+						<Collapse in={userChecked}>
+							<Divider sx={styles.divider} />
+							<Grid
+								container
+								sx={styles.box}
+							>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<List>
+										<ListItem>
+											<ListItemIcon>
+												<PersonIcon />
+											</ListItemIcon>
+											<ListItemText primary='Tarkastele käyttäjiä' />
+										</ListItem>
+										<ListItem>
+											<ListItemIcon>
+												<PersonIcon />
+											</ListItemIcon>
+											<ListItemText primary='Luo käyttäjiä' />
+										</ListItem>
+										<ListItem>
+											<ListItemIcon>
+												<PersonIcon />
+											</ListItemIcon>
+											<ListItemText primary='Muokkaa ja poista käyttäjiä' />
+										</ListItem>
+									</List>
+								</Grid>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<Link
+										to={'/userControl'}
+										style={styles.link}
+									>
+										<Button
+											variant='contained'
+											sx={styles.button}
+										>
+											Siirry
+										</Button>
+									</Link>
+								</Grid>
+							</Grid>
+						</Collapse>
+					</Paper>
+					<Paper sx={styles.paper}>
+						<Box
+							onClick={() =>
+								setCreateAreaChecked(!createAreaChecked)
+							}
+						>
+							<Typography
+								variant='h6'
+								sx={styles.subText}
+							>
+								Luo alue
+								<IconButton sx={styles.icon}>
+									{createAreaChecked ? (
+										<RemoveIcon />
+									) : (
+										<AddIcon />
+									)}
+								</IconButton>
+							</Typography>
+						</Box>
+
+						<Collapse in={createAreaChecked}>
+							<Divider sx={styles.divider} />
+							<Grid
+								container
+								sx={styles.box}
+							>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<List>
+										<ListItem>
+											<ListItemIcon>
+												<MapIcon />
+											</ListItemIcon>
+											<ListItemText primary='Luo uusia alueita' />
+										</ListItem>
+									</List>
+								</Grid>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<Link
+										to={'/createArea'}
+										style={styles.link}
+									>
+										<Button
+											variant='contained'
+											sx={styles.button}
+										>
+											Siirry
+										</Button>
+									</Link>
+								</Grid>
+							</Grid>
+						</Collapse>
+					</Paper>
+					<Paper sx={styles.paper}>
+						<Box onClick={() => setLendChecked(!lendChecked)}>
+							<Typography
+								variant='h6'
+								sx={styles.subText}
+							>
+								Lainaa
+								<IconButton sx={styles.icon}>
+									{lendChecked ? <RemoveIcon /> : <AddIcon />}
+								</IconButton>
+							</Typography>
+						</Box>
+
+						<Collapse in={lendChecked}>
+							<Divider sx={styles.divider} />
+							<Grid
+								container
+								sx={styles.box}
+							>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<List>
+										<ListItem>
+											<ListItemIcon>
+												<MapIcon />
+											</ListItemIcon>
+											<ListItemText primary='Hallitse alueiden lainaus pyyntöjä' />
+										</ListItem>
+									</List>
+								</Grid>
+								<Grid
+									item
+									md={12}
+									xs={12}
+								>
+									<Link
+										to={'/lendList'}
+										style={styles.link}
+									>
+										<Button
+											variant='contained'
+											sx={styles.button}
+										>
+											Siirry
+										</Button>
+									</Link>
+								</Grid>
+							</Grid>
+						</Collapse>
+					</Paper>
 				</Grid>
 				<Grid
 					item
 					md={4}
 					xs={12}
 				>
-					<Typography
-						variant='h6'
-						sx={styles.subText}
-					>
-						Aloitus
-					</Typography>
-					<Typography sx={styles.text}>
-						Etkö ole vielä rekisteröitynyt?
-					</Typography>
-					<Link
-						to={'/register'}
-						style={styles.link}
-					>
-						<Button
-							variant='contained'
-							sx={styles.button}
+					<Paper sx={styles.paper}>
+						<Typography
+							variant='h6'
+							sx={styles.subText}
 						>
-							Rekisteröidy
-						</Button>
-					</Link>
-					<Typography sx={styles.text}>
-						Kirjaudu sisään jos olet jo rekisteröitynyt!
-					</Typography>
-					<Link
-						to={'/login'}
-						style={styles.link}
-					>
-						<Button
-							variant='contained'
-							sx={styles.button}
+							Aloitus
+						</Typography>
+						<Typography sx={styles.text}>
+							Etkö ole vielä rekisteröitynyt?
+						</Typography>
+						<Link
+							to={'/register'}
+							style={styles.link}
 						>
-							Kirjaudu sisään
-						</Button>
-					</Link>
+							<Button
+								variant='contained'
+								sx={styles.button}
+							>
+								Rekisteröidy
+							</Button>
+						</Link>
+						<Typography sx={styles.text}>
+							Kirjaudu sisään jos olet jo rekisteröitynyt!
+						</Typography>
+						<Link
+							to={'/login'}
+							style={styles.link}
+						>
+							<Button
+								variant='contained'
+								sx={styles.button}
+							>
+								Kirjaudu sisään
+							</Button>
+						</Link>
+					</Paper>
 				</Grid>
 			</Grid>
 		</Container>

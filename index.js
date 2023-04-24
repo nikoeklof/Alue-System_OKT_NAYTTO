31b9b463d31115cbe09dfc6a6f711c1297f8d4b5
Niko / Nigey <dataaja.96@hotmail.com>
2023-04-24 15:25:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const { ApolloServer } = require("@apollo/server")
-const { startStandaloneServer } = require("@apollo/server/standalone")
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const schema = require("./execSchema");
+const resolvers = require("./resolvers");
+const typeDefs = require("./schema");
 const User = require("./models/user");
 
-const mongoUrl = process.env.MONGODB_URI
-console.log("Connecting to", mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
+console.log("Connecting to", mongoUrl);
 
 mongoose
   .connect(mongoUrl)
   .then(() => {
-    console.log("Connected to MongoDB")
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.log("Error connection to MongoDB:", error.message)
-  })
+    console.log("Error connection to MongoDB:", error.message);
+  });
 
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
   plugins: [
     {
       requestDidStart(requestContext) {
         return {
           didResolveOperation({ operation, operationName }) {
             if (process.env.LOGGING === "1") {
-              if (operationName === "IntrospectionQuery")
-                return null
+              if (operationName === "IntrospectionQuery") return null;
 
               console.log("***");
               console.log("Operation type: " + operation.operation);
@@ -42,24 +43,25 @@ const server = new ApolloServer({
               console.log(
                 "Variables: " + JSON.stringify(requestContext.request.variables)
               );
+              console.log("***");
             }
-          }
-        }
-      }
-    }
+          },
+        };
+      },
+    },
   ],
-})
+});
 
 startStandaloneServer(server, {
   listen: { port: process.env.PORT },
   context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null
+    const auth = req ? req.headers.authorization : null;
     if (auth) {
       const decodedToken = jwt.verify(auth, process.env.JWT_SECRET);
       const authUser = await User.findById(decodedToken.id);
-      if (!authUser.rank.disabled) return { authUser };
+      if (!authUser.disabled) return { authUser };
     }
   },
 }).then(({ url }) => {
-  console.log("Server ready at", url)
-})
+  console.log("Server ready at", url);
+});
